376d6688b5f9c221f08a3b924b28b71d7f934ee1
Greenlio <49602706+Sodapopping@users.noreply.github.com>
2023-05-27 20:10:35 -0500

    lol

---
+++ README.md
@@ -0,0 +1,10 @@
+# xQcUpdates Automation
+
+This project provides real-time updates on xQc's Twitch stream title, whether he's live or not, and the game he's currently playing. It is designed to keep you informed about xQc's streaming activities.
+
+## Technologies Used
+
+- Node.js: JavaScript runtime environment
+- Discord.js: Discord JavaScript framework in Node.js
+- Twitch API: Retrieve xQc's Twitch stream information
+- Axios: Enables requests from local system to Twitch's API
---
+++ index-no-discord.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+require('dotenv').config();
+
+let isStreamOnline = false;
+let streamGame = "Intro";
+
+checkStreamStatus();
+setInterval(checkStreamStatus, 15000);
+
+async function checkStreamStatus() {
+	try {
+		const response = await axios.get(`https://api.twitch.tv/helix/streams?user_login=xqc`, {
+			headers: {
+				'Client-ID': process.env.TWITCH_CLIENT_ID,
+				'Authorization': process.env.TWITCH_TOKEN,
+			},
+		});
+
+		let stream = response.data.data[0];
+
+		if (stream) {
+
+			if(!streamGame == stream.game_name && isStreamOnline) {
+				if(streamGame == "Intro" && stream.game_name == "Just Chatting") return; 
+				console.log(`\nGame changed from ${streamGame} to ${stream.game_name}\n`);
+				streamGame = stream.game_name;
+			}
+
+			if (!isStreamOnline) {
+				console.log(`\nxQc is live on Twitch!\n\nTitle: ${stream.title}\nGame: ${stream.game_name}\nStream Link: https://twitch.tv/xQc`);
+				isStreamOnline = true;
+			}
+		} else {
+			isStreamOnline = false;
+			console.log('\nStreamer now offline.\n')
+		}
+	} catch (error) { null }
+} // Well, that was easy.
